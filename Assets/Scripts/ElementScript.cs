26f63f45ca7479e6a90cf1e4456d87071597839b
Tin Huynh <tinn_1998@yahoo.com>
2014-12-27 17:27:04 -0800

    Add elements and their script
    When you click on the element it will add its name to the queue.

---
+++ Assets/Scripts/ElementScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElementScript : MonoBehaviour {
+    public string name;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	   
+	}
+    void OnMouseDown() {
+        Global.inputs.Add(name);
+        foreach (string s in Global.inputs) {
+            Debug.Log(s);
+        }
+    }
+}
---
+++ Assets/Scripts/Global.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Global : MonoBehaviour {
+    public static int FPS = 60;
+    public static ArrayList inputs = new ArrayList();
+
+
+	void Start () {
+        Application.targetFrameRate = FPS;
+	}
+}
